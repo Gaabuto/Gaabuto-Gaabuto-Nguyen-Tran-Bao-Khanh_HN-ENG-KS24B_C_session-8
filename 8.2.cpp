2290725970d9b59cc9c33b6f38818cd1cad5287c
Gaabuto <khanh040604@gmail.com>
2024-11-29 00:42:36 +0700

    Update 8.2.cpp

--- 8.2.cpp
+++ 8.2.cpp
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int main(){
-	int a,i, random, ar[5]= {1,2,3,4,5};
+	int a,b,i, random, ar[5]= {1,2,3,4,5};
 	printf("moi ban nhap 1 so bat ki: ");
 	scanf("%d", &random);
 	for(i = 0; i < sizeof(ar)/sizeof(int); i++){
-	if (random != ar[i]){
-		printf("so ban chon khong nam trong tap hop cua toi !!!");
-	}else{
+	if (random == ar[i]){
+		b ++;
 		printf("so ban chon nam trong tap hop cua toi va no o vi tri thu %d !!!", i + 1);
 		break;
+		
 	}
+	if(b == 0){
+		printf("so ban chon khong nam trong tap hop cua toi !!!");
+}
 }
 	return 0;
 }
